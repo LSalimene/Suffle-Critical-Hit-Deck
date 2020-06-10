9de67f6932e795ea6c4fb7e207a95407eb94457e
Lucas Salimene <lucassalimene@gmail.com>
2020-06-10 11:54:28 -0300

    Create randcart.py
    The first code that I make for this project, this is the basic idea

---
+++ randcart.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random as rdm
+#generate a ramdom number
+n = rdm.randrange(1,55)
+#chose with deck is be used
+critic = input("If you roll a critical hit type S, for a critical fumble type F: ")
+#suffle the deck using the ramdom number
+if (critic=='S'):
+  img=mpimg.imread('/s'+str(n)+'.JPG')
+  imgplot=plt.imshow(img)
+  plt.axis('off')
+  plt.show()
+elif (critic=="F"):
+  img=mpimg.imread('f'+str(n)+'.JPG')
+  imgplot=plt.imshow(img)
+  plt.axis('off')
+  plt.show()
